73f052478283af2174e046ee988cd46c49f269a9
Renato Aero <renatoaero.moura@gmail.com>
2021-04-27 02:09:57 -0300

    Adição do ícone e dos pacotes

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Index from "./src/components";
+const { Home, Media } = Index;
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            title: "Home",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: '#FF0000',
+            },
+            headerTintColor: '#FFF',
+          }}
+        />
+        <Stack.Screen
+          name="Media"
+          component={Media}
+          options={{
+            title: "Média de Consumo",
+            headerTitleAlign: "left",
+            headerStyle: {
+              backgroundColor: '#FF0000',
+            },
+            headerTintColor: '#FFF',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
